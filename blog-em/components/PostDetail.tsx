10a4894776724ef5b12c22dc72284ff77ed423db
Miller <farwes1180@gmail.com>
2022-12-26 00:58:19 +0900

    query-keys

--- blog-em/components/PostDetail.tsx
+++ blog-em/components/PostDetail.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { FC, Fragment } from "react";
 import { PostInterface } from "./Posts";
 
@@ -11,8 +12,52 @@ interface Props {
   post: PostInterface;
 }
 
+const fetchComments = async (postId: number): Promise<CommentInterface[]> => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+
+  return response.json();
+};
+
+const deletePost = async (postId: number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+
+  return response.json();
+};
+
+const updatePost = async (postId: number, title: string) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({ title }),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    }
+  );
+
+  return response.json();
+};
+
 const PostDetail: FC<Props> = ({ post }) => {
-  const data: CommentInterface[] = [];
+  const { data, isError, isLoading, error } = useQuery(
+    ["comment", post.id],
+    () => fetchComments(post.id)
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError) {
+    return (
+      <Fragment>
+        <h3>Oops, something went wrong!</h3>
+        {typeof error === "string" ? <p>{error.toString()}</p> : <Fragment />}
+      </Fragment>
+    );
+  }
 
   return (
     <Fragment>
