66f9b2c2e7b64c888535648be737a3fc8fb7fd71
Miller <farwes1180@gmail.com>
2022-12-27 07:25:34 +0900

    query-keys

--- blog-em/components/PostDetail.tsx
+++ blog-em/components/PostDetail.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { FC, Fragment } from "react";
 import { PostInterface } from "./Posts";
 
@@ -11,8 +12,30 @@ interface Props {
   post: PostInterface;
 }
 
+const fetchComments = async (postId: number): Promise<CommentInterface[]> => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+
+  return response.json();
+};
+
 const PostDetail: FC<Props> = ({ post }) => {
-  const data: CommentInterface[] = [];
+  const { data, isError, isLoading, error } = useQuery(
+    ["comment", post.id],
+    () => fetchComments(post.id)
+  );
+
+  if (isLoading) return <h3>Loading ...</h3>;
+
+  if (isError) {
+    return (
+      <Fragment>
+        <h3>Oops, something went wrong!</h3>
+        {typeof error === "string" ? <p>{error.toString()}</p> : <Fragment />}
+      </Fragment>
+    );
+  }
 
   return (
     <Fragment>
