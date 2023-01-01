8eb9c636dd8f4ad253223d1647c2df7cf35b79d9
Miller <farwes1180@gmail.com>
2023-01-02 00:28:53 +0900

    add react-query & react-query-devtools

--- infinite-swapi/src/App.tsx
+++ infinite-swapi/src/App.tsx
@@ -1,14 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FC } from "react";
 import InfinitePeople from "./people/InfinitePeople";
 import InfiniteSpecies from "./species/InfiniteSpecies";
 
+const queryClient = new QueryClient();
+
 const App: FC = () => {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      <InfiniteSpecies />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        <InfiniteSpecies />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
