3613e57a6d845bd85cd520831b7c3b6bc55dcb2d
Miller <farwes1180@gmail.com>
2023-01-10 23:31:02 +0900

    useInfiniteQuery complete

--- infinite-swapi/src/people/InfinitePeople.tsx
+++ infinite-swapi/src/people/InfinitePeople.tsx
@@ -1,6 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import InfiniteScroll from "react-infinite-scroller";
+import Person from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url: string) => {
@@ -8,8 +9,29 @@ const fetchUrl = async (url: string) => {
   return response.json();
 };
 
+interface ApiResponse {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: ResultsInterface[];
+}
+
+interface ResultsInterface {
+  name: string;
+  hair_color: string;
+  eye_color: string;
+}
+
 const InfinitePeople: FC = () => {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+  } = useInfiniteQuery(
     ["sw-people"],
     ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     {
@@ -17,8 +39,31 @@ const InfinitePeople: FC = () => {
     }
   );
 
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll loadMore={() => {}} />;
+  if (isLoading) {
+    return <div className="loading">Loading ...</div>;
+  }
+
+  if (isError) {
+    return <div>Error! {(error as Error).toString()}</div>;
+  }
+
+  return (
+    <Fragment>
+      {isFetching && <div className="loading">Loading ...</div>}
+      <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage}>
+        {data.pages.map((pageData: ApiResponse) =>
+          pageData.results.map((item, index) => (
+            <Person
+              key={`person_${item.name}_${index}`}
+              name={item.name}
+              hairColor={item.hair_color}
+              eyeColor={item.eye_color}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </Fragment>
+  );
 };
 
 export default InfinitePeople;
